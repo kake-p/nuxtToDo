0879d7cec0b175f135a43770f84581172cba0790
kake <Makky.rrr.omach@yahoo.ne.jp>
2024-03-26 15:24:05 +0900

    overall-style

--- pages/index.vue
+++ pages/index.vue
@@ -4,11 +4,38 @@ import TodoListView from '../components/TodoListView.vue';
 </script>
 
 <template>
-  <div>
+  <h1>To Do Task Manager</h1>
+  <section>
+    <div>
     <client-only>
       <TodoInput />
       <TodoListView />
-    </client-only>
-    
-  </div>  
+    </client-only>    
+  </div> 
+  </section>   
 </template>
+
+<style scoped>
+  h1{
+    margin: 0;
+    text-align: center;
+    font-size: 48px;
+    background: rgba(00, 99, 82, .5); 
+    color: white; 
+    border-top:   2px solid black;
+    border-bottom: 2px solid black;
+    line-height: 96px;
+  }
+
+  section{
+    height: 50vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  div{
+    font-size: 16px;
+    text-align: center;
+  }
+</style>
